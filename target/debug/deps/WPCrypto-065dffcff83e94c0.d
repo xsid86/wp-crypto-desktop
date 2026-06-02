b56a4b70fa92b54c9c2d93b8f5991ad7ed575757
xsid86 <shvets.jan@gmail.com>
2026-06-02 15:37:35 +0400

    initial commit

---
+++ target/debug/deps/WPCrypto-065dffcff83e94c0.d
@@ -0,0 +1,5 @@
+C:\Users\aveni\www\wpcrypto.loc\desktop\WPCrypto\target\debug\deps\WPCrypto-065dffcff83e94c0.d: src\main.rs
+
+C:\Users\aveni\www\wpcrypto.loc\desktop\WPCrypto\target\debug\deps\libWPCrypto-065dffcff83e94c0.rmeta: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/WPCrypto-66e1fccf66b77137.d
@@ -0,0 +1,5 @@
+C:\Users\aveni\www\wpcrypto.loc\desktop\WPCrypto\target\debug\deps\WPCrypto-66e1fccf66b77137.d: src\main.rs
+
+C:\Users\aveni\www\wpcrypto.loc\desktop\WPCrypto\target\debug\deps\libWPCrypto-66e1fccf66b77137.rmeta: src\main.rs
+
+src\main.rs:
